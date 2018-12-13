ece24272947c1fcb6f7e41a0c76d90bf130bd3e2
Michel <michelpl@yahoo.com.br>
2018-12-13 00:00:48 -0200

    :sparkles: Add more columns on footer.

--- wp-content/themes/invsc/footer.php
+++ wp-content/themes/invsc/footer.php
@@ -17,6 +17,7 @@
                     </div><!-- .widget-area -->
                 <?php endif; ?>
             </div>
+
             <div class="float-left box">
                 <h4>Localização</h4>
                 <hr>
@@ -33,27 +34,50 @@
                     <li><span class="text-muted">Pr Mauricio Lopes Fortunato: mauricio@invsc.org.br</span></li>
                 </ul>
             </div>
-            <!--div class="float-left box">
-                <h4>Contato</h4>
-                <hr>
-                <ul class="list-unstyled">
-                    <li><p>Telefones</p></li>
-                    <li><span class="text-muted">Tel: (21)3890-3867</span></li>
-                    <li><span class="text-muted">Fax: (21)2585-1227</span></li>
-                    <li><p>E-mails</p></li>
-                    <li><span class="text-muted">Informações: invsc@invsc.org.br</span></li>
-                    <li><span class="text-muted">Pr Mauricio Lopes Fortunato: mauricio@invsc.org.br</span></li>
-                </ul>
-            </div-->
-            <div class="float-left box full-width">
-                <h4>Programação semanal</h4>
-                <hr>
-                <ul class="list-unstyled">
-                    <li><p>Domingos</p></li>
-                    <li><span class="text-muted">Contato</span></li>
-                    <li><span class="text-muted">Mais um link</span></li>
-                </ul>
+            <div class="float-left" style="width: 100%;">
+                <div class="float-left box full-width">
+                    <h4>Domingos</h4>
+                    <hr>
+                    <ul class="list-unstyled">
+                        <li><span class="text-muted">9:00h - EBD dos jovens (3º andar)</span></li>
+                        <li><span class="text-muted">9:30h - EBD adultos (templo)</span></li>
+                        <li><span class="text-muted">9:30h - EBD doutrinas básicas (2º andar)</span></li>
+                        <li><span class="text-muted">10:30h - Culto</span></li>
+                        <li><span class="text-muted">19:00h - Culto</span></li>
+                        <li><p>1º domingo do mês</p></li>
+                        <li><span class="text-muted">- Ceia e entrega dos alimentos para a cesta básica nos dois cultos</span></li>
+                        <li><span class="text-muted">- 8:00h 1º domingo do mês - Consagração dos ministérios</span></li>
+                    </ul>
+
+                </div>
+                <div class="float-left box full-width">
+                    <h4>Terças</h4>
+                    <hr>
+                    <ul class="list-unstyled">
+                        <li><span class="text-muted">19:30h Culto da família</span></li>
+                        <li><p>1º terça do mês</p></li>
+                        <li><span class="text-muted">- Ceia e entrega dos alimentos para a cesta básica</span></li>
+                    </ul>
+                </div>
+                <div class="float-left box full-width">
+                    <h4>Quintas</h4>
+                    <hr>
+                    <ul class="list-unstyled">
+                        <li><span class="text-muted">19:30h Culto</span></li>
+                        <li><p>1º quinta do mês</p></li>
+                        <li><span class="text-muted">- Ceia e entrega dos alimentos para a cesta básica</span></li>
+                    </ul>
+                </div>
+                <div class="float-left box full-width">
+                    <h4>Sábados</h4>
+                    <hr>
+                    <ul class="list-unstyled">
+                        <li><span class="text-muted">9:00h - EBD dos jovens (3º andar)</span></li>
+                    </ul>
+                </div>
             </div>
+
+
         </div>
     </div>
     <div class="container">
