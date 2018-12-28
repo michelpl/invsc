35a10a38c99b7884cfe596d8dfce772a455a9cba
Michel <michelpl@yahoo.com.br>
2018-12-28 01:22:15 -0200

    :art: Improve comments.

--- wp-content/themes/invsc/comments.php
+++ wp-content/themes/invsc/comments.php
@@ -6,41 +6,60 @@ if ( post_password_required() ) {
 ?>
 <div class="row" id="comment-box">
     <div class="panel panel-default col-lg-12">
-        <div class="panel-heading"><h3>Comentários para este post <span class="comment-number">(1)</span></h3></div>
+        <div class="panel-heading"><h3>Comentários para este post <span class="comment-number">(<?php echo get_comments_number(); ?>)</span></h3></div>
         <div class="panel-body">
             <?php
-            // You can start editing here -- including this comment!
             if ( have_comments() ) : ?>
-                <h2 class="comments-title">
-                    <?php
-                    $comments_number = get_comments_number();
-                    if ( '1' === $comments_number ) {
-                        /* translators: %s: post title */
-
-                    } else {
-
-                            number_format_i18n( $comments_number );
-                    }
-                    ?>
-                </h2>
-
                 <ul class="comment-list">
                     <?php foreach ($comments as $comment) { ?>
+
                          <li>
-                            <div class="image-box">
-                                <img src="<?php echo get_avatar_url($comment->comment_author_email); ?>">
-                            </div>
-                            <div class="comment-header">
-                                <span class="author-name">
-                                    <?php echo $comment->comment_author; ?>
-                                </span>
-                                <span class="date">
-                                    <?php echo $comment->comment_date; ?>
-                                </span>
-                            </div>
-                            <div class="content">
-                                <?php echo $comment->comment_content; ?>
-                            </div>
+                            <?php if($comment->comment_parent == 0) { ?>
+                                <div class="bg-white row padding-comment">
+                                    <div class="image-box col-lg-2">
+                                        <img src="<?php echo get_avatar_url($comment->user_id); ?>">
+                                    </div>
+                                    <div class="col-lg-10">
+                                        <div class="comment">
+                                            <div class="author-name">
+                                                <?php echo get_the_author_meta('nickname', $comment->user_id);?>
+                                            </div>
+                                            <div class="date">
+                                                Em <?php echo $comment->comment_date; ?>
+                                            </div>
+                                            <div class="text">
+                                                <?php echo $comment->comment_content; ?>
+                                            </div>
+
+                                            <a href="/post-mais/?replytocom=<?php echo $comment->comment_ID; ?>#respond">Responder</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php } else { ?>
+                                <div class="row">
+                                    <div class="col-lg-1 padding-comment" style="">
+                                        <img class="float-right" src="<?php echo get_theme_file_uri( 'assets/images/reply-arrow.png' ); ?>" width="18px">
+                                    </div>
+                                    <div class="image-box col-lg-2 bg-white padding-comment">
+                                        <img src="<?php echo get_avatar_url($comment->user_id); ?>">
+                                    </div>
+                                    <div class="col-lg-9 bg-white padding-comment">
+                                        <div class="comment">
+                                            <div class="author-name">
+                                                <?php echo get_the_author_meta('nickname', $comment->user_id);?>
+                                            </div>
+                                            <div class="date">
+                                                Em <?php echo $comment->comment_date; ?>
+                                            </div>
+                                            <div class="text">
+                                                <?php echo $comment->comment_content; ?>
+                                            </div>
+
+                                            <a href="/post-mais/?replytocom=<?php echo $comment->comment_ID; ?>#respond">Responder</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php } ?>
                          </li>
                     <?php } ?>
                 </ul>
